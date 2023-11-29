cd8644e9d63386e5892c2016a84136a0ca7530b6
woo-be <thsckddn100@gmail.com>
2023-11-29 20:39:41 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -4,12 +4,11 @@ import java.util.Scanner;
 
 public class App {
 
-  // 애플리케이션 클래스 App을 실행할 때 다음 변수를 미리 준비해 둔다.
-  static final String ANSI_CLEAR = "\033[0m";
-  static final String ANSI_BOLD_RED = "\033[1;31m";
-  static final String ANSI_RED = "\033[0;31m";
-  static final String APP_TITLE = ANSI_BOLD_RED + "[과제관리 시스템]" + ANSI_CLEAR;
-  static final String[] MENUS = {
+  static String ANSI_CLEAR = "\033[0;0m";
+  static String ANSI_RED = "\033[0;31m";
+  static String ANSI_BOLD_RED = "\033[1;31m";
+  static String APP_TITLE = "[과제관리 시스템]";
+  static String[] MENUS = {
       "1. 과제",
       "2. 게시글",
       "3. 도움말",
@@ -17,15 +16,16 @@ public class App {
   };
 
   public static void main(String[] args) {
-    printMenu();
 
+    printMenu();
+    System.out.println('\u0041');
     java.util.Scanner keyIn = new java.util.Scanner(System.in);
-    
+
     loop:
     while (true) {
-      String menuNo = prompt(keyIn);
+      String input = prompt(keyIn);
 
-      switch (menuNo) {
+      switch (input) {
         case "1":
           System.out.println("과제입니다.");
           break;
@@ -36,12 +36,9 @@ public class App {
           System.out.println("도움말입니다.");
           break;
         case "4":
-          System.out.println("종료합니다.\n");
-          System.out.println("--------------------------------------");
+          System.out.println("종료합니다.");
           break loop;
         case "menu":
-          // 코드를 기능 단위로 묶어 메서드로 정의하면
-          // 메서드의 이름을 통해 해당 기능을 쉽게 유추할 수 있어 유지보수에 좋다.
           printMenu();
           break;
         default:
@@ -49,18 +46,11 @@ public class App {
           break;
       }
     }
-
     keyIn.close();
   }
 
   static void printMenu() {
-    // ANSI  코드와 App 제목, 메뉴를 저장한 변수를 메서드 안에 두는 대신에
-    // 클래스 블록 안에 두면
-    // printMenu()를 호출할 때마다 변수를 만들지 않기 때문에 실행 속도나 메모리 부분에서
-    // 훨씬 효율적이다.
-    // 보통 메서드 호출될 때 마다 값이 바뀌는 변수가 아니라 고정 값을 갖는 변수인 경우
-    // 메서드 밖에 두는 것이 좋다.
-    System.out.println(APP_TITLE);
+    System.out.println(ANSI_BOLD_RED + APP_TITLE + ANSI_CLEAR);
     System.out.println();
     for (String menu : MENUS) {
       System.out.println(menu);
@@ -71,5 +61,4 @@ public class App {
     System.out.print("> ");
     return keyIn.nextLine();
   }
-
 }
