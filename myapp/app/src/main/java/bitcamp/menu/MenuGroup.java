e983ec45f63b4618353699632a7c8ffe00539f7a
woo-be <thsckddn100@gmail.com>
2023-12-12 20:02:36 +0900

    20231212

--- myapp/app/src/main/java/bitcamp/menu/MenuGroup.java
+++ myapp/app/src/main/java/bitcamp/menu/MenuGroup.java
@@ -1,7 +1,9 @@
-package bitcamp.myapp.menu;
+package bitcamp.menu;
 
 import bitcamp.util.Prompt;
 
+// Composite 패턴에서 '복합 객체(composite object)' 역할을 하는 클래스
+// - 다른 Menu 객체를 포함한다.
 public class MenuGroup implements Menu {
 
   String title;
@@ -12,8 +14,9 @@ public class MenuGroup implements Menu {
     this.title = title;
   }
 
+  @Override // 인터페이스나 수퍼 클래스의 메서드를 정의하겠다고 컴파일러에게 알린다.
   public void execute(Prompt prompt) {
-    printMenu();
+    this.printMenu();
 
     while (true) {
       String input = prompt.input("%s> ", this.title);
@@ -35,12 +38,19 @@ public class MenuGroup implements Menu {
     }
   }
 
-  public void printMenu() {
+  private void printMenu() {
     System.out.printf("[%s]\n", this.title);
 
     for (int i = 0; i < this.menuSize; i++) {
       System.out.printf("%d. %s\n", (i + 1), menus[i].getTitle());
     }
+
+    System.out.printf("0. %s\n", "이전");
+  }
+
+  @Override
+  public String getTitle() {
+    return this.title;
   }
 
   public void add(Menu menu) {
@@ -58,7 +68,24 @@ public class MenuGroup implements Menu {
     this.menus[this.menuSize++] = menu;
   }
 
-  public String getTitle() {
-    return this.title;
+  public void remove(Menu menu) {
+    int index = this.indexOf(menu);
+    if (index == -1) {
+      return;
+    }
+
+    for (int i = index; i < (this.menuSize - 1); i++) {
+      this.menus[i] = this.menus[i + 1];
+    }
+    this.menus[--this.menuSize] = null;
+  }
+
+  int indexOf(Menu menu) {
+    for (int i = 0; i < menuSize; i++) {
+      if (menu == this.menus[i]) {
+        return i;
+      }
+    }
+    return -1;
   }
 }
