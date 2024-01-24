786dc31059144044b9985f3ef4887279769eb63f
woo-be <thsckddn100@gmail.com>
2024-01-24 12:30:23 +0900

    .

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam03/Main.java
@@ -0,0 +1,18 @@
+package algorithm.test.baekjoon.level06.exam03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+//    bw.flush();
+//    bw.close();
+  }
+}
