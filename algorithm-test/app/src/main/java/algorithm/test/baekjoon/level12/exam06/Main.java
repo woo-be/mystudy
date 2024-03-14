88a0c2ac8b1bdbb60a56eed4f8804f0e837555c1
woo-be <thsckddn100@gmail.com>
2024-03-14 11:18:42 +0900

    .

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level12/exam06/Main.java
@@ -0,0 +1,37 @@
+package algorithm.test.baekjoon.level12.exam06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(br.readLine());
+    int cnt = 0;
+    int weightSum = 0;
+
+    while (weightSum != n) {
+
+    }
+
+    System.out.println(cnt);
+
+    br.close();
+    // bw.flush();
+    // bw.close();
+  }
+}
+
+// 3 4 5 6 7 8 9 10 11 12 13 14 15
+// 5 3 4 0 1 2 3 4 0 1 2 3 4 0
+// 3 0 1 2 0 1 2 0 1 2 0 1 2 0
+//
+//
+//
+//
+//
